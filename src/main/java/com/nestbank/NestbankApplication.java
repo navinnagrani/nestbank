24133655f0820cf9a6cd8a0595d78d0e644e0b1b
Navin Nagrani <74898439+navinnagrani@users.noreply.github.com>
2026-01-29 11:26:51 +0530

    Initial Commit

---
+++ src/main/java/com/nestbank/NestbankApplication.java
@@ -0,0 +1,13 @@
+package com.nestbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestbank/NestbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
